42abe73bfc23f6d0eb62f22b41e3d928e27c1171
Simon Bubendorf <simon.bubendorf@baloise.ch>
2019-12-27 08:56:25 +0100

    New tries on PDF Creator

--- src/ch/rusi/sandbox/itext/CreatePDF.java
+++ src/ch/rusi/sandbox/itext/CreatePDF.java
@@ -1,10 +1,12 @@
 package ch.rusi.sandbox.itext;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.AreaBreak;
 
 public class CreatePDF {
 	
@@ -14,10 +16,17 @@ public class CreatePDF {
 	 *	http://www.tutorialspoint.com/itext/index.htm
 	 * 
 	 */
+	
+	private static PdfWriter writer = null;
+	private static PdfDocument pdfDoc  = null;
+	private static Document document = null;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		String destination = "output/sample.pdf";
+		
+		createDocument("01_empty_one_pager.pdf");
+		
 		
 		try {
 			
@@ -25,13 +34,16 @@ public class CreatePDF {
 			PdfWriter writer = new PdfWriter(destination);
 			
 			// Creating the PDF document
-			PdfDocument doc = new PdfDocument(writer);
-			
-			// Adding a new page
-			doc.addNewPage();
+			PdfDocument pdfDoc = new PdfDocument(writer);
 			
 			// Creating a Document
-			Document document = new Document(doc);
+			Document document = new Document(pdfDoc);
+			
+			// Creating an Area Break
+			AreaBreak ab = new AreaBreak();
+			
+			// Adding the area break to the PDF
+			document.add(ab);
 			
 			// Closing the document
 			document.close();
@@ -44,6 +56,20 @@ public class CreatePDF {
 		}
 		
 
+	}
+	
+	private static void createDocument(String fileName) throws FileNotFoundException {
+
+		// Creating the PDF Writer
+		PdfWriter writer = new PdfWriter("output/" + fileName);
+		
+		// Creating the PDF document
+		PdfDocument pdfDoc = new PdfDocument(writer);
+		pdfDoc.addNewPage();
+		
+		// Creating a Document
+		document = new Document(pdfDoc);
+		
 	}
 
 }
