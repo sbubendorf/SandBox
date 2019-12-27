ab659ef82e14e5480d373615d5b49b949b469ac5
Simon Bubendorf <simon.bubendorf@baloise.ch>
2019-12-27 15:20:31 +0100

    More iText examples added

--- src/ch/rusi/sandbox/itext/CreatePDF.java
+++ src/ch/rusi/sandbox/itext/CreatePDF.java
@@ -1,12 +1,19 @@
 package ch.rusi.sandbox.itext;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.AreaBreak;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.List;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.thedeanda.lorem.LoremIpsum;
 
 public class CreatePDF {
 	
@@ -21,54 +28,92 @@ public class CreatePDF {
 	private static PdfDocument pdfDoc  = null;
 	private static Document document = null;
 
+	private static final String FILE_EXT = ".pdf"; 
+
 	public static void main(String[] args) throws IOException {
 		
-		String destination = "output/sample.pdf";
+		document = createDocument("01_empty_one_pager");
+		document.close();
 		
-		createDocument("01_empty_one_pager.pdf");
+		// --------------------------------------------------------------------------------
 		
-		
-		try {
-			
-			// Creating the PDF Writer
-			PdfWriter writer = new PdfWriter(destination);
-			
-			// Creating the PDF document
-			PdfDocument pdfDoc = new PdfDocument(writer);
-			
-			// Creating a Document
-			Document document = new Document(pdfDoc);
-			
-			// Creating an Area Break
-			AreaBreak ab = new AreaBreak();
-			
-			// Adding the area break to the PDF
-			document.add(ab);
-			
-			// Closing the document
-			document.close();
-			
-			System.out.println("Empty PDF document successfully created!");
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		document = createDocument("02_area_break");
+		AreaBreak ab = new AreaBreak();
+		document.add(ab);
+		document.close();
 
+		// --------------------------------------------------------------------------------
+		
+		document = createDocument("03_paragraph");
+		Paragraph para1 = new Paragraph(LoremIpsum.getInstance().getParagraphs(1, 1));
+		Paragraph para2 = new Paragraph(LoremIpsum.getInstance().getParagraphs(1, 1));
+		document.add(para1);
+		document.add(para2);
+		document.close();
+
+		// --------------------------------------------------------------------------------
+		
+		document = createDocument("04_list");
+		Paragraph para = new Paragraph("Check out the following list of Lorem Ipsum texts:");
+		document.add(para);
+		List list = new List();
+		for (int i = 0 ; i < 10 ; i++) {
+			list.add(LoremIpsum.getInstance().getWords(2, 5));
+		}
+		document.add(list);
+		document.close();
+
+		// --------------------------------------------------------------------------------
+		
+		document = createDocument("05_table");
+		float[] colWidths = {150F,150F,150F};
+		ArrayList<String> lastNames = new ArrayList<String>();
+		ArrayList<String> firstNames = new ArrayList<String>();
+		ArrayList<String> cities = new ArrayList<String>();
+		for (int i = 0 ; i < 10 ; i++) {
+			lastNames.add(LoremIpsum.getInstance().getLastName());
+			firstNames.add(LoremIpsum.getInstance().getFirstName());
+			cities.add(LoremIpsum.getInstance().getCity());
+		}
+		Table tab = new Table(colWidths);
+		tab.addCell(new Cell().add("Last name"));
+		for (String lastName : lastNames) {
+			tab.addCell(new Cell().add(lastName));
+		}
+		tab.addCell(new Cell().add("First name"));
+		for (String firstName : firstNames) {
+			tab.addCell(new Cell().add(firstName));
+		}
+		tab.addCell(new Cell().add("City"));
+		for (String city : cities) {
+			tab.addCell(new Cell().add(city));
+		}
+		document.add(tab);
+		document.close();
+		
 	}
 	
-	private static void createDocument(String fileName) throws FileNotFoundException {
+	private static Document createDocument(String fileName) throws FileNotFoundException {
+	
+		fileName = fileName.toLowerCase();
+		if (!fileName.endsWith(FILE_EXT)) {
+			fileName += FILE_EXT;
+		}
+		File file = new File("output/" + fileName);
 
 		// Creating the PDF Writer
-		PdfWriter writer = new PdfWriter("output/" + fileName);
+		writer = new PdfWriter(file.getAbsolutePath());
 		
 		// Creating the PDF document
-		PdfDocument pdfDoc = new PdfDocument(writer);
+		pdfDoc = new PdfDocument(writer);
+		
+		// The new document has at least one empty page
 		pdfDoc.addNewPage();
 		
+		System.out.println("New file created : " + file.getAbsolutePath());
+		
 		// Creating a Document
-		document = new Document(pdfDoc);
+		return new Document(pdfDoc);
 		
 	}
 
